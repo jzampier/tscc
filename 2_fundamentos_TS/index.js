eb6f1e25cef51492bf0393537d4f769def82489b
Julio Zampier <juliozampier@gmail.com>
2023-06-10 18:57:47 -0300

    Fundamentos TS done

--- 2_fundamentos_TS/index.js
+++ 2_fundamentos_TS/index.js
@@ -1,26 +1,23 @@
+"use strict";
 //!Primitivos
 //!1 - numbers let/const variavel:number
-let num: number = 1000;
-let num2: number = 15.454;
+let num = 1000;
+let num2 = 15.454;
 console.warn('numero:', num, 'numero2:', num2.toPrecision(4));
-
 //!2 - strings let/const variavel:string
-let nome: string = 'Nosdaj';
-let sobrenome: string = 'Arievilo';
-let fullName: string;
+let nome = 'Nosdaj';
+let sobrenome = 'Arievilo';
+let fullName;
 fullName = nome + ' ' + sobrenome;
 console.log(fullName.toUpperCase(), typeof fullName);
-
 //!3 - boolean let/const variavel:boolean, nao aceita mais 0 / 1
-let boolBoy: boolean = true;
+let boolBoy = true;
 boolBoy = false;
-
 //* TS e a aplicacao
 // pair programming, alguem que nos ajuda a programar
-
 //? Type annotation (explicita) e Type inference (implicito)
 //? type annotation
-let ann: string = 'teste';
+let ann = 'teste';
 console.log(ann, typeof ann);
 //? type inference
 let inf = 'teste';
