82d7d8bbf1111abd2c7f0d0b53fc5830f1fec193
Julio Zampier <juliozampier@gmail.com>
2023-06-10 21:13:49 -0300

    a10 - union type

--- 3_avancando_tipos/index.js
+++ 3_avancando_tipos/index.js
@@ -1,3 +1,4 @@
+"use strict";
 //! Avancando em tipos
 /*
 ! Arrays
@@ -10,11 +11,70 @@ let variavel: Array<tipo>
 const numbers: Array<number>
 const names: Array<string>
  */
-var numbers = [1, 2, 3, 4];
-console.log("LOG ~ numbers:", numbers);
-var names = ['Jao', 'Nosdaj', 'Pedroka'];
-console.log("LOG ~ names:", names);
-var numAndNames = [1, 2, 'Jao', 'Nosdaj', 'Pedroka'];
-console.log("LOG ~ numAndNames:", numAndNames);
+const numbers = [1, 2, 3, 4];
+console.log(`LOG ~ numbers:`, numbers);
+const names = ['Jao', 'Nosdaj', 'Pedroka'];
+console.log(`LOG ~ names:`, names);
+const numAndNames = [1, 2, 'Jao', 'Nosdaj', 'Pedroka'];
+console.log(`LOG ~ numAndNames:`, numAndNames);
 names.push('Arievilo');
-console.log("LOG ~ names:", names);
+console.log(`LOG ~ names:`, names);
+//! Tipo Any , nao recomendado pois vai de encontro a ideia fundamental do TS
+let anything = 'Jao';
+console.log(typeof anything);
+const arrAny = [1, 'Jao', true, { nome: 'Jao' }];
+console.log(`LOG ~ arrAny:`, arrAny);
+//! Tipo de parametro de funcoes
+/*
+ * myFunction(param: tipo){}
+ * myFunction(name: string, age: number){}
+ */
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 2));
+//! Tipando o retorno da funcao
+/*
+ * myFunction(param: tipo): tipo{}
+ */
+function greeting(name) {
+    return `Ola ${name}`;
+}
+console.log(greeting('Jao'));
+//! Tipando funcoes anonimas
+// setTimeout(() => {
+//   const salary:number = 1000
+//   console.log(parseFloat(salary)); erro, o parseFloat eh metodo para string
+// }, 2000);
+//! Tipando objetos
+/* {prop:tipo, prop2:tipo2} */
+function passCoordinates(coord) {
+    console.log('Latitude:' + coord.lat);
+    console.log('Longitude:' + coord.long);
+}
+const myCoordinates = { lat: 329, long: -123 };
+passCoordinates(myCoordinates);
+//! Propriedades opcionais
+//* {nome:string, sobrenome?:string}
+function showNumbers(a, b, c) {
+    console.log('A: ' + a);
+    console.log('B: ' + b);
+    if (c) {
+        console.log('C: ' + c);
+    }
+}
+showNumbers(1, 2);
+showNumbers(1, 2, 3);
+//! Validar props adicionais
+/*
+o primeiro paramentro nao pode ser opcional, os opcionais devem ficar para o
+final da lista de parametros
+ */
+function advancedGreeting(name, lastName) {
+    if (lastName !== undefined) {
+        return `Ola ${name} ${lastName}`;
+    }
+    return `Ola ${name}`;
+}
+console.log(advancedGreeting('Jao'));
+console.log(advancedGreeting('Jao', 'Nosdaj'));
--- 3_avancando_tipos/index.ts
+++ 3_avancando_tipos/index.ts
@@ -20,3 +20,70 @@ const numAndNames: (string | number)[] = [1, 2, 'Jao', 'Nosdaj', 'Pedroka'];
 console.log(`LOG ~ numAndNames:`, numAndNames);
 names.push('Arievilo');
 console.log(`LOG ~ names:`, names);
+
+//! Tipo Any , nao recomendado pois vai de encontro a ideia fundamental do TS
+let anything: any = 'Jao';
+console.log(typeof anything);
+
+const arrAny: any[] = [1, 'Jao', true, { nome: 'Jao' }];
+console.log(`LOG ~ arrAny:`, arrAny);
+
+//! Tipo de parametro de funcoes
+/*
+ * myFunction(param: tipo){}
+ * myFunction(name: string, age: number){}
+ */
+function sum(a: number, b: number) {
+  return a + b;
+}
+console.log(sum(1, 2));
+
+//! Tipando o retorno da funcao
+/*
+ * myFunction(param: tipo): tipo{}
+ */
+function greeting(name: string): string {
+  return `Ola ${name}`;
+}
+console.log(greeting('Jao'));
+
+//! Tipando funcoes anonimas
+// setTimeout(() => {
+//   const salary:number = 1000
+//   console.log(parseFloat(salary)); erro, o parseFloat eh metodo para string
+// }, 2000);
+
+//! Tipando objetos
+/* {prop:tipo, prop2:tipo2} */
+function passCoordinates(coord: { lat: number; long: number }) {
+  console.log('Latitude:' + coord.lat);
+  console.log('Longitude:' + coord.long);
+}
+const myCoordinates = { lat: 329, long: -123 };
+passCoordinates(myCoordinates);
+
+//! Propriedades opcionais
+//* {nome:string, sobrenome?:string}
+function showNumbers(a: number, b: number, c?: number) {
+  console.log('A: ' + a);
+  console.log('B: ' + b);
+  if (c) {
+    console.log('C: ' + c);
+  }
+}
+showNumbers(1, 2);
+showNumbers(1, 2, 3);
+
+//! Validar props adicionais
+/*
+o primeiro paramentro nao pode ser opcional, os opcionais devem ficar para o
+final da lista de parametros
+ */
+function advancedGreeting(name: string, lastName?: string) {
+  if (lastName !== undefined) {
+    return `Ola ${name} ${lastName}`;
+  }
+  return `Ola ${name}`;
+}
+console.log(advancedGreeting('Jao'));
+console.log(advancedGreeting('Jao', 'Nosdaj'));
