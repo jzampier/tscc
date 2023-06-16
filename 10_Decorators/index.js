2abfb67d1b2a771e271b6561fc969d9752abd465
Julio Zampier <juliozampier@gmail.com>
2023-06-16 17:32:24 -0300

    Multiplos decorators

--- 10_Decorators/index.js
+++ 10_Decorators/index.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 //! Decorators (usados para customizar algum metodo ou funcao)
 //! @nomeDaFuncao
 //! Lembrar de ajustar o tsconfig.json para que o decorators funcione
@@ -7,10 +13,29 @@
  * Grande vantagem:  permite localizar onde foi executado o decorator
  */
 //! 1 Exemplo de decorator
+function myDecorator() {
+    //* 1 a ser executado
+    console.log('Fui executado - function myDecorator');
+    //? sintaxe basica do decorator
+    return function (
+    //* 2 a ser executado
+    target, propertyKey, descriptor) {
+        console.log('Decorator em execucao, cheguei no return function');
+        console.log(target);
+        console.log(propertyKey);
+        console.log(descriptor);
+    };
+}
 class Myclass {
+    //? Chamando o decorator
     testing() {
+        //* ultimo a ser executado
         console.log('Terminando a execucao do metodo');
     }
 }
+__decorate([
+    myDecorator()
+], Myclass.prototype, "testing", null);
 const myObj = new Myclass();
 myObj.testing();
+//! 2 Multiplos decorators
--- 10_Decorators/index.ts
+++ 10_Decorators/index.ts
@@ -6,11 +6,32 @@
  * Grande vantagem:  permite localizar onde foi executado o decorator
  */
 //! 1 Exemplo de decorator
+function myDecorator() {
+  //* 1 a ser executado
+  console.log('Fui executado - function myDecorator');
+  //? sintaxe basica do decorator
+  return function (
+    //* 2 a ser executado
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log('Decorator em execucao, cheguei no return function');
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+  };
+}
 class Myclass {
+  //? Chamando o decorator
+  @myDecorator()
   testing(): void {
+    //* ultimo a ser executado
     console.log('Terminando a execucao do metodo');
   }
 }
 
 const myObj = new Myclass();
 myObj.testing();
+
+//! 2 Multiplos decorators
