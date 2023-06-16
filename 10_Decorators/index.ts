72a850ae4a0bf1866397854ebccdc92219822540
Julio Zampier <juliozampier@gmail.com>
2023-06-16 16:50:29 -0300

    Decorators 1o exemplo

---
+++ 10_Decorators/index.ts
@@ -0,0 +1,16 @@
+//! Decorators (usados para customizar algum metodo ou funcao)
+//! @nomeDaFuncao
+//! Lembrar de ajustar o tsconfig.json para que o decorators funcione
+/*
+ * Pode trabalhar com argumentos especiais (target, propertyKey, descriptor)
+ * Grande vantagem:  permite localizar onde foi executado o decorator
+ */
+//! 1 Exemplo de decorator
+class Myclass {
+  testing(): void {
+    console.log('Terminando a execucao do metodo');
+  }
+}
+
+const myObj = new Myclass();
+myObj.testing();
